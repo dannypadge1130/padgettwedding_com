56359488c5dd4531d848cd05b6298cf32ec3dfd6
Dan Padgett <dan.padgett1@gmail.com>
2016-02-21 14:01:51 -0500

    fix countdown counter. attemp to fix GA tags

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ COMMING SOON PAGE
     * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
     * according to the GMT+0 Timezone
     **/
-    var launch = new Date(2016, 6, 3, 4, 0);
+    var launch = new Date(2016, 6, 03, 24, 30);
     /**
     * The script
     **/
